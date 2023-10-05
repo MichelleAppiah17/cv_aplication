2f3fbc4f578883c4ff59e7058de561485d7f804c
Michelle Appiah <michappiah67@gmail.com>
2023-10-05 09:27:00 +0000

    education experience inputs

--- src/EduExperience.js
+++ src/EduExperience.js
@@ -1,8 +1,85 @@
-// EducationalExperience.js
-import React from 'react';
 
-function EducationalExperience({ educationalExperience, setEducationalExperience }) {
-  // Add logic to add, edit, and delete educational experiences
+import React, {useState,useEffect} from 'react';
+
+function EducationalExperience(props){
+  const [school, setSchool] = useState('')
+  const [degree, setDegree] = useState('')
+  const [startYear, setStartyear] = useState('')
+  const [endYear, setEndyear] = useState('')
+  const [location, setLocation] = useState('')
+
+  useEffect(() => {
+    if(props.isEditing){
+      setSchool(props.infoToEdit.school);
+      setDegree(props.infoToEdit.degree);
+      setStartyear(props.infoToEdit.startYear);
+      setEndyear(props.infoToEdit.startYear);
+      setLocation(props.infoToEdit.location)
+    }
+  },[props.isEditing,props.infoToEdit]);
+
+  const isFormValid = () =>{
+    return school.length && degree.length && startYear.length && endYear.length && location.length
+  }
+  
+  return(
+    <div className='educationalExperience'>
+      <div className='eduExperienceDiv'>
+        <div className='formSectionTitle'>
+          <p>Educational Experience</p>
+        </div>
+      </div>
+      <form>
+        <div className='inputContainer'>
+          <label htmlFor='school'>School</label>
+          <input
+            type='text'
+            id='school'
+            name='school'
+            placeholder='Enter name of school'
+          />
+        </div>
+        <div className='inputContainer'>
+          <label htmlFor='degree'>Degree</label>
+          <input
+             type='text'
+             id='degree'
+             name='degree'
+             placeholder='Enter name of degree'
+          />
+        </div>
+        <div className='inputContainer'>
+          <label htmlFor='startYear'>Start Year</label>
+          <input
+             type='year'
+             id='startYear'
+             name='startYear'
+             placeholder='Enter Start Year'
+          />
+        </div>
+        <div className='inputContainer'>
+          <label htmlFor='endYear'>End Year</label>
+          <input
+             type='year'
+             id='endYear'
+             name='endYear'
+             placeholder='Enter End Year'
+          />
+        </div>
+        <div className='inputContainer'>
+          <label htmlFor='location'>Location</label>
+          <input
+             type='text'
+             id='location'
+             name='location'
+             placeholder='Enter Location'
+          />
+        </div>
+      </form>
+
+    </div>
+  )
+  
 }
 
 export default EducationalExperience;
